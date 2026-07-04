e8f8f63250e6adf730e8f06b91bab83b1b408086
Hilmi Fauzi Adha Simamora <adhahilmi17@gmail.com>
2026-07-04 19:01:48 +0700

    fix: fix RogueCanvas's black flicker when resizing the viewport

--- RogueCanvas/src/EditorLayer.cpp
+++ RogueCanvas/src/EditorLayer.cpp
@@ -74,6 +74,16 @@ namespace Rogue
 	{
 		RE_PROFILE_FUNCTION();
 
+		// Resize
+		if (
+			Rogue::FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+			m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f && // zero sized framebuffer is invalid
+			(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+		{
+			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+			m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+		}
+
 		// Update
 		if (m_ViewportFocused)
 			m_CameraController.OnUpdate(ts);
@@ -203,21 +213,11 @@ namespace Rogue
 		m_ViewportHovered = ImGui::IsWindowHovered();
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
 
-		RE_CORE_WARN("Focused: {0}", ImGui::IsWindowFocused());
-		RE_CORE_WARN("Hovered: {0}", ImGui::IsWindowHovered());
-
 		ImVec2 viewportPanelSize{ ImGui::GetContentRegionAvail() };
+		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
+
 		uint32_t textureID{ m_Framebuffer->GetColorAttachmentRendererID() };
-
-		if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize))
-		{
-			m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
-			m_Framebuffer->Resize((uint32_t)viewportPanelSize.x, (uint32_t)viewportPanelSize.y);
-
-			m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
-		}
-
-		ImGui::Image(textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 
 		ImGui::End();
 		ImGui::PopStyleVar();
